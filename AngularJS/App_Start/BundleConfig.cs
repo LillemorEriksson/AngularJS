2bcdd52063ab47db68d01a114d870c70cbffedd7
Lillemor Eriksson <lillemor_eira@hotmail.com>
2018-04-12 16:26:13 +0200

    0412

--- AngularJS/App_Start/BundleConfig.cs
+++ AngularJS/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace AngularJS
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/angular.js"));
+        
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
